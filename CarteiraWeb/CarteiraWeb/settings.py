cc42585bbd2c349ea1e0e8e4d95d2d77b3541a89
Igor Santos <igor.seferino@gmail.com>
2023-11-06 20:33:45 -0300

    Update settings.py

--- CarteiraWeb/CarteiraWeb/settings.py
+++ CarteiraWeb/CarteiraWeb/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = ['*','18.191.233.250','0.0.0.0','https://www.easycontrol.site/',
 
 CSRF_TRUSTED_ORIGINS = ['https://easycontrol.site','https://www.easycontrol.site','http://3.13.2.175','http://0.0.0.0']
 
+CSRF_COOKIE_SECURE = False
+
 # Application definition
 
 INSTALLED_APPS = [
